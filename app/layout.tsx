b2e9dd1938e907b7fe2f8761561e83a6076d4307
Fahad4345 <fahadrehan723@gmail.com>
2026-08-14 11:37:06 +0500

    Changes to be committed: 	modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import ReduxProvider from "@/components/ReduxProvider";
 import Navbar from "@/components/Navbar";
@@ -20,7 +21,24 @@ export default function RootLayout({
       <body
         className="min-h-screen text-[#f0f0f0]"
         style={{ fontFamily: "'Outfit', sans-serif" }}
-      >
+      ><Script
+  id="meta-pixel"
+  strategy="afterInteractive"
+>
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+    fbq('track', 'PageView');
+  `}
+</Script>
         <ReduxProvider>
           <Toaster />
           <Navbar />
